d9398943278d6cf4b9c7f636ced1b249830f5af6
chakki-the-potato <chanhee2468@gmail.com>
2026-08-29 23:41:13 +0900

    chore: 수식과 cask를 v0.6.1로 올린다

--- Formula/restage.rb
+++ Formula/restage.rb
@@ -8,8 +8,8 @@
 class Restage < Formula
   desc "Restore a declared layout of apps and windows in one step"
   homepage "https://github.com/chakki-the-potato/restage"
-  url "https://github.com/chakki-the-potato/restage/archive/refs/tags/v0.6.0.tar.gz"
-  sha256 "623094da5e5086697e006bb474ff59a6708b3719245a41a2296c9a4c35623a91"
+  url "https://github.com/chakki-the-potato/restage/archive/refs/tags/v0.6.1.tar.gz"
+  sha256 "8b981e45942ea9ad2d053328c07ac1d752e2bb20bbb33c672630a115a10d852a"
   license "MIT"
   head "https://github.com/chakki-the-potato/restage.git", branch: "main"
 
